fff4bf20320c303b41daaa96d59d25922f0c8d7b
Azuby <83681090+Azuby-hash@users.noreply.github.com>
2023-02-16 16:32:03 +0700

    main.cpp

--- ESP/src/main/main.cpp
+++ ESP/src/main/main.cpp
@@ -52,29 +52,67 @@ void setup() {
 
   // Smart config wifi và xác định địa chỉ IP của server
   wifiSmartConfig();
-
-  // Đo nhiệt độ và lưu giá trị
-  sensors.requestTemperatures(); 
-  temperatureC = sensors.getTempCByIndex(0);
 }
 
 void loop() {
-  if (millis() - _time > 1000) {
+  if (millis() - _time > 500) {
     _time = millis();
 
-    // String request để đẩy giá trị lên server
-    String reqString = "http://" + server + "/espPost";
+    // Đo nhiệt độ và lưu giá trị
+    sensors.requestTemperatures(); 
+    temperatureC = sensors.getTempCByIndex(0);
+    if (temperatureC < 0.1) { 
+      return;
+    }
+
+    // String request để đẩy giá trị lên server ------------------------------
+    String reqString1 = "http://" + server + "/espPost";
 
     // JSON chứa index node hiện tại, giá trị và bảo mật cần gửi
-    String json = "{\"number\":" + (String)NODE_ID + ",\"password\":\"9797964a-2f5c-41c6-91c1-44aa68308631\", \"value\":" ;
-    json = json + temperatureC;
-    json = json + "}";
+    String json1 = "{\"number\":" + (String)NODE_ID + ",\"password\":\"9797964a-2f5c-41c6-91c1-44aa68308631\", \"value\":" ;
+    json1 = json1 + temperatureC;
+    json1 = json1 + "}";
 
     // Request post lên server
-    String resString = httpPOSTRequest(reqString, json);
+    String resString1 = httpPOSTRequest(reqString1, json1);
 
     // In ra response trả về từ server
-    Serial.println(resString);
+    Serial.println(resString1);
+
+    // String request để lấy giá trị từ server -------------------------------
+    String reqString2 = "http://" + server + "/espGet";
+
+    // JSON chứa bảo mật cần gửi
+    String json2 = "{\"number\":" + (String)NODE_ID + ",\"password\":\"9797964a-2f5c-41c6-91c1-44aa68308631\", \"isSP\": true}" ;
+
+    // Request post lên server
+    String resString2 = httpPOSTRequest(reqString2, json2);
+    
+    // Response trả về setpoint, convert ra float
+    deserializeJson(doc, resString2);
+    float sp = doc.as<float>();
+
+    // So sánh với giá trị hiện tại
+    if (sp <= temperatureC) {
+      digitalWrite(25, HIGH);
+      digitalWrite(26, LOW);
+      digitalWrite(27, LOW);
+    }
+
+    if (sp > temperatureC && (sp - temperatureC) < 10) {
+      digitalWrite(25, LOW);
+      digitalWrite(26, HIGH);
+      digitalWrite(27, LOW);
+    }
+
+    if (sp > temperatureC && (sp - temperatureC) < 20) {
+      digitalWrite(25, LOW);
+      digitalWrite(26, LOW);
+      digitalWrite(27, HIGH);
+    }
+
+    // In ra response trả về từ server
+    Serial.println(resString2);
 
     // Xóa bộ nhớ và ngủ
     Serial.flush(); 
